b562684bb099d153853cb45cb455ee60e056bdac
Maksym Mostovenko <maksim@local.com>
2021-08-09 14:53:44 +0300

    MM: json edited

---
+++ task1/Dictionaries.py
@@ -0,0 +1,5 @@
+# Merge several dictionaries into one, named as CommonDictionary. Software 
+# should be able to sort dictionaries "A-Z" and "Z-A". Software should 
+# print KEY that points to tree highest values.
+
+import open_task_json
