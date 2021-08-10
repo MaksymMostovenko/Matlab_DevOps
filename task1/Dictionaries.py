449458bf3ee4ecddcf22d919fbfff59bbc655c03
Maksym Mostovenko <blockmaksim@gmail.com>
2021-08-10 18:15:03 +0300

    MM: Script draft completed

--- task1/Dictionaries.py
+++ task1/Dictionaries.py
@@ -1,27 +1,59 @@
-# Merge several dictionaries into one, named as CommonDictionary. Software
-# should be able to open files  in bulk. Software # should be able to sort 
-# dictionaries "A-Z" and "Z-A". Software should # print KEY that points to 
-# three highest values.
+# Merge several dictionaries into one, named as CommonDictionaryionary. 
+# Software should be able to open files in bulk. Software should be able 
+# to sort dictionaries "A-Z" and "Z-A". Software should print KEYS that 
+# points to three highest values.
 
 import open_task_json
 import os
+import operator
+
+def SortDicionary(dictionary, arg):
+    SortedDictionary = {"Goods":{}}
+    SortedKeys = sorted(dictionary["Goods"], key=lambda x: (dictionary["Goods"][x]["Price"]))
+    
+    if arg == "Ascending":
+        for K in SortedKeys:
+            SortedDictionary["Goods"].update({K:dictionary["Goods"][K]})
+    elif arg == "Descending":
+        for K in reversed(SortedKeys):
+            SortedDictionary["Goods"].update({K:dictionary["Goods"][K]})
+    else:
+        try:
+            raise NameError(SortDicionary.__name__,"Wrong argument", arg,)
+        except NameError:
+            
+            print("*** ERROR ***", SortDicionary.__name__, "*** ERROR ***")
+            raise
+    
+    return SortedDictionary
+
+def HighestValues(dictionary):
+    KeyList = list(dictionary["Goods"].keys())
+    if dictionary["Goods"][KeyList[0]]["Price"] > dictionary["Goods"][KeyList[-1]]["Price"]:
+        for n in range(3):
+            print(KeyList[n])
+    else:
+        for n in range(1,4):
+            print(KeyList[-n])
 
 WORKDIRR = os.getcwd()
+# Configs
+
+#Containers
 JSFileList = []
-Commondict = {}
+CommonDictionary = {}
 
 for file in os.listdir(WORKDIRR):
     if file.endswith(".json"):
         JSFileList.append(file)
 
 for FileName in JSFileList:
-    dictionary = dict(open_task_json.OpenJSON(FileName))
-    if not bool(Commondict):
-        Commondict.update(dictionary)
+    dictionary = open_task_json.OpenJSON(FileName)
+    if not bool(CommonDictionary):
+        CommonDictionary.update(dictionary)
     else:
-        Commondict["Goods"] |= dictionary["Goods"]
+        CommonDictionary["Goods"] |= dictionary["Goods"]
 
-# get data fom goods item
-for goods, g_info, in Commondict.items():
-    for key in g_info:
-        print(g_info[key]["Price"])
+# Sort dictionary
+CommonDictionary = SortDicionary(CommonDictionary, "Ascending")
+HighestValues(CommonDictionary)
